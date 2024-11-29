8575fe431c36b3f8ec3613a8eeaf2c33b9a7d71e
Yuu-Yur <gkcjdqls@gmail.com>
2024-11-29 16:37:11 +0900

    주입방식 대세는 constructor injection

--- SpringBasic/springPractice/src/main/java/com/busanit501/practice/sample/SampleService.java
+++ SpringBasic/springPractice/src/main/java/com/busanit501/practice/sample/SampleService.java
@@ -1,20 +1,30 @@
 package com.busanit501.practice.sample;
 
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
 
-// 컨트롤러 역할 @Controller
-// 서비스 역할 @Service
-// DAO 역할 @Repository
-@Component // 일반 역할 객체 타입
+@Service
 @ToString
+//@RequiredArgsConstructor
 public class SampleService {
 //    기존이라면
 //    SampleDAO sampleDao = new SampleDAO();
 //    로 사용할텐데
-    @Autowired
-    private SampleDAO sampleDAO;
+//    1)필드 주입
+//    @Autowired
+//    private SampleDAO sampleDAO;
+//    2)생성자 주입
+//    2-1)기본 생성자 주입
+    private final SampleDAO sampleDAO;
+    public SampleService(SampleDAO sampleDAO) {
+        this.sampleDAO = sampleDAO;
+    }
+//    2-2)lombok 애너테이션 이용
+//    annotation RAC RequiredArgsConstructor 필요
+//    private final SampleDAO sampleDAO;
+
+
 }
