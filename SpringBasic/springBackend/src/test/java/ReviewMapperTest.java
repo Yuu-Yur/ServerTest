37a7d0467bb3416bd3dc8e6544c547da31361ede
Yuu-Yur <gkcjdqls@gmail.com>
2024-12-17 12:57:24 +0900

    user mapper 단위테스트 완료 user service 만들고 단위테스트 movie read 에서 user service 부르게 , view 작업 필요

--- SpringBasic/springBackend/src/test/java/ReviewMapperTest.java
+++ SpringBasic/springBackend/src/test/java/ReviewMapperTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Log4j2
 @ExtendWith(SpringExtension.class)
@@ -19,11 +20,13 @@ public class ReviewMapperTest {
 
     @Test
     public void test1() {
-        reviewMapper.insert(ReviewVO.builder()
-                .userName("테스트")
-                .title("The Future Begins")
-                .content("리뷰매퍼 삽입 테스트중")
-                .build());
+        IntStream.range(1, 31).forEach(i -> {
+            reviewMapper.insert(ReviewVO.builder()
+                    .userName("테스트")
+                    .title("The Future Begins")
+                    .content("리뷰매퍼 삽입 테스트중" + i)
+                    .build());
+        });
     }
 
     @Test
@@ -43,13 +46,26 @@ public class ReviewMapperTest {
 
     @Test
     public void test4() {
-        int result = reviewMapper.count(PageRequestDTO.builder().page(5).pageSize(10).build());
+        int result = reviewMapper.count(PageRequestDTO.builder()
+                .page(5)
+                .size(10)
+                .types(new String[]{"c"})
+                .keyword("5")
+                .build());
         log.info(result);
     }
 
     @Test
     public void test5() {
-        List<ReviewVO> result = reviewMapper.select(PageRequestDTO.builder().page(5).pageSize(10).build());
+        PageRequestDTO prdto = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .types(new String[]{"c"})
+                .keyword("5")
+                .build();
+        log.info(prdto);
+        List<ReviewVO> result = reviewMapper.select(prdto);
+        if (result.size() == 0) {log.info("List 없음 , mapper 문제");}
         log.info(result);
     }
 }
